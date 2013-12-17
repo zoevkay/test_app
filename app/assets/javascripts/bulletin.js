5c8cc5518d06d1d86b14e027c07946043d5603d1
Jason Staten <jstaten07@gmail.com>
2013-12-16 17:33:58 -0700

    Add XHR header so Rails will give back JSON
    Rails ignores content negotiation when the Accept header includes `*/*`.
    By adding the XHR header for Angular requests, it'll fix the behavior.

--- app/assets/javascripts/bulletin.js
+++ app/assets/javascripts/bulletin.js
@@ -1,4 +1,7 @@
 var bulletinApp = angular.module('bulletinApp', ['ngResource']);
+bulletinApp.config(['$httpProvider', function ($httpProvider) {
+  $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+}]);
 
   bulletinApp.controller('PostsCtrl', ['$scope', 'Post', function($scope, Post) {
     $scope.welcome_text = 'Welcome to the bulletin board. LOSER.';
